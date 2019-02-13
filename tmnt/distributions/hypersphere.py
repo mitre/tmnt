d220a283087c470485ee27c7dc5ffbaa2626526b
Wellner <wellner@.mitre.org>
2019-02-13 18:30:22 -0500

    Working on hyperspherical dist

--- tmnt/distributions/hypersphere.py
+++ tmnt/distributions/hypersphere.py
@@ -2,25 +2,39 @@
 
 import math
 import mxnet as mx
+from scipy import special as sp
+from mxnet import gluon
+from tmnt.distributions import LatentDistribution
 
 __all__ = ['HyperSphericalUniform']
 
-class HyperSphericalUniform(object):
+class HyperSphericalUniform(LatentDistribution):
 
     def __init__(self, dim, ctx):
-        self.dim = dim
-        self.ctx = ctx
+        super(HyperSphericalUniform, self).__init__(n_latent, ctx)
+        self.kappa = 1.0
+        
+        with self.name_scope():
+            self.mu_encoder = gluon.nn.Dense(units = n_latent, activation=None)
 
-    def get_entropy(self):
-        return _log_surface_area(self)
 
     def _log_surface_area(self):
-        return math.log(2) + ((self._dim + 1) / 2) * math.log(math.pi) - \
-            mx.nd.gammaln(mx.nd.array([(self._dim + 1) / 2], ctx=self.ctx))
+        return math.log(2) + ((self.n_latent + 1) / 2) * math.log(math.pi) - \
+            mx.nd.gammaln(mx.nd.array([(self.n_latent + 1) / 2], ctx=self.ctx))
 
-    def log_prob(self, x):
-        return mx.nd.ones(x.shape[:-1], ctx = self.ctx) * self._log_surface_area
 
-    def sample(self, in_shape):
-        output = mx.ndarray.random.normal(0, 1, shape=in_shape + (dim+1,))
-        return output / output.norm(axis=-1, keepdims=True)
+    def hybrid_forward(self, F, data):
+        mu = self.mu_encoder(data)
+        norm = F.norm(mu, axis=1)
+        mu = mu / norm
+        return mu
+
+    @staticmethod
+    def _vmf_kld(k, d):
+        tmp = (k * ((sp.iv(d / 2.0 + 1.0, k) + sp.iv(d / 2.0, k) * d / (2.0 * k)) / sp.iv(d / 2.0, k) - d / (2.0 * k)) \
+               + d * np.log(k) / 2.0 - np.log(sp.iv(d / 2.0, k)) \
+               - sp.loggamma(d / 2 + 1) - d * np.log(2) / 2).real
+        if tmp != tmp:
+            exit()
+        return np.array([tmp])
+
