Fixed bug with normalization of word vectors

evaluate.py
@@ -71,25 +71,28 @@ if __name__ == "__main__":
 
     top_k_words_per_topic = get_top_k_word_idx_per_topic(inference_model, 5, 5)
 
-    labels, words = read_vector_file(args.eval_file)
+    #labels, words = read_vector_file(args.eval_file)
 
-    words = [
-        [inference_model.vocab.idx_to_token[i] for i in doc] for doc in words
-    ]
+    #words = [
+    #    [inference_model.vocab.idx_to_token[i] for i in doc] for doc in words
+    #]
 
     #inference_model.model.hybridize(static_alloc=True)
 
-    encoded = inference_model.encode_texts(words)
+    #encoded = inference_model.encode_texts(words)
 
+    
+    encoded, labels = inference_model.encode_vec_file(args.eval_file)
     encodings = np.array([doc.asnumpy() for doc in encoded])
+    
 
     print("There are {0} labels and {1} encodings".format(len(labels), len(encodings)))
 
-    umap_model = umap.UMAP(n_neighbors=25, min_dist=0.4)
+    umap_model = umap.UMAP(n_neighbors=5, min_dist=0.1, metric='euclidean')
     embeddings = umap_model.fit_transform(encodings)
     print(embeddings.shape)
 
-    plt.scatter(*embeddings.T, c=labels, s=0.1, alpha=1.0, cmap='Spectral')
+    plt.scatter(*embeddings.T, c=labels, s=0.2, alpha=0.9, cmap='Spectral')
     plt.savefig("something.png", dpi=1000)
 
     unigram_reader = UnigramReader(args.vocab_file)

tmnt/bow_runner.py
@@ -21,7 +21,7 @@ def get_wd_freqs(data_csr, n_terms, max_sample_size=10000):
     data = data_csr[:max_sample_size].asnumpy() # only take first 10000 to estimate frequencies
     sums = np.sum(data, axis=0)
     return list(sums)
-    
+
 
 def train(args, vocabulary, data_train_csr, total_tr_words, data_test_csr=None, total_tst_words=0, train_labels=None, test_labels=None, ctx=mx.cpu()):
     wd_freqs = get_wd_freqs(data_train_csr, len(vocabulary))
@@ -31,15 +31,15 @@ def train(args, vocabulary, data_train_csr, total_tr_words, data_test_csr=None, 
         train_labels = mx.nd.one_hot(train_labels, n_covars)
         test_labels = mx.nd.one_hot(test_labels, n_covars) if test_labels is not None else None
         model = \
-            MetaDataBowNTM(n_covars,vocabulary, args.hidden_dim, args.n_latent, emb_size, latent_distrib=args.latent_distribution,
-               coherence_reg_penalty=args.coherence_regularizer_penalty,
-               batch_size=args.batch_size, wd_freqs=wd_freqs, ctx=ctx)
+            MetaDataBowNTM(n_covars,vocabulary, args.hidden_dim, args.n_latent, emb_size,
+                           fixed_embedding=args.fixed_embedding, latent_distrib=args.latent_distribution,
+                           coherence_reg_penalty=args.coherence_regularizer_penalty,
+                           batch_size=args.batch_size, wd_freqs=wd_freqs, ctx=ctx)
     else:
         model = \
-            BowNTM(vocabulary, args.hidden_dim, args.n_latent, emb_size, latent_distrib=args.latent_distribution,
-               coherence_reg_penalty=args.coherence_regularizer_penalty,
-               batch_size=args.batch_size, wd_freqs=wd_freqs, ctx=ctx)
-    
+            BowNTM(vocabulary, args.hidden_dim, args.n_latent, emb_size, fixed_embedding=args.fixed_embedding, latent_distrib=args.latent_distribution,
+                   coherence_reg_penalty=args.coherence_regularizer_penalty,
+                   batch_size=args.batch_size, wd_freqs=wd_freqs, ctx=ctx)
     train_iter = mx.io.NDArrayIter(data_train_csr, train_labels, args.batch_size, last_batch_handle='discard', shuffle=True)
     train_dataloader = DataIterLoader(train_iter)    
     if data_test_csr is not None:
@@ -122,6 +122,10 @@ def train_bow_vae(args):
     if args.embedding_source:
         glove_twitter = nlp.embedding.create('glove', source=args.embedding_source)
         vocab.set_embedding(glove_twitter)
+        emb_size = len(vocab.embedding.idx_to_vec[0])
+        for word in vocab.embedding._idx_to_token:
+            if (vocab.embedding[word] == mx.nd.zeros(emb_size)).sum() == emb_size:
+                vocab.embedding[word] = mx.nd.random.normal(-1.0, 1.0, emb_size)
         
     ### XXX - NOTE: For smaller datasets, may make sense to convert sparse matrices to dense here up front
     m = train(args, vocab, tr_csr_mat, total_tr_words, tst_csr_mat, total_tst_words, tr_labels, tst_labels, ctx=ctx)

tmnt/bow_vae/bow_models.py
@@ -22,7 +22,7 @@ class BowNTM(HybridBlock):
     batch_size : int (default None) provided only at training time (or when model is Hybridized) - otherwise will be inferred
     ctx : context device (default is mx.cpu())
     """
-    def __init__(self, vocabulary, enc_dim, n_latent, embedding_size, latent_distrib='logistic_gaussian',
+    def __init__(self, vocabulary, enc_dim, n_latent, embedding_size, fixed_embedding=False, latent_distrib='logistic_gaussian',
                  init_l1=0.0, coherence_reg_penalty=0.0, batch_size=None, wd_freqs=None, ctx=mx.cpu()):
         super(BowNTM, self).__init__()
         self.batch_size = batch_size
@@ -38,7 +38,7 @@ class BowNTM(HybridBlock):
                                       shape=(1,),
                                       init=mx.init.Constant([init_l1]), 
                                       differentiable=False)
-            self.embedding = gluon.nn.Dense(in_units=self.vocab_size, units=self.embedding_size, activation = 'relu')
+            self.embedding = gluon.nn.Dense(in_units=self.vocab_size, units=self.embedding_size, activation='tanh')
             self.encoder = gluon.nn.Dense(units = enc_dim, activation='softrelu') ## just single FC layer 'encoder'
             if latent_distrib == 'logistic_gaussian':
                 self.latent_dist = LogisticGaussianLatentDistribution(n_latent, ctx)
@@ -50,12 +50,16 @@ class BowNTM(HybridBlock):
             self.decoder = gluon.nn.Dense(in_units=n_latent, units=self.vocab_size, activation=None)
             self.coherence_regularization = CoherenceRegularizer(coherence_reg_penalty)
         self.initialize(mx.init.Xavier(), ctx=self.model_ctx)
-        if vocabulary.embedding:
+        if vocabulary.embedding:            
             emb = vocabulary.embedding.idx_to_vec.transpose()
-            emb_norm_val = mx.nd.norm(emb, keepdims=True, axis=1)
-            emb_norm = mx.nd.broadcast_div(emb, emb_norm_val)
+            #g_noise = mx.nd.random.normal(loc=0.0, scale=0.1, shape=emb_1.shape)
+            #emb = emb_1 + g_noise
+            emb_norm_val = mx.nd.norm(emb, keepdims=True, axis=0) + 1e-10
+            emb_norm = emb / emb_norm_val
+            #print("Emb norm = {} with sum = {}".format(emb_norm, emb_norm.sum()))
             self.embedding.weight.set_data(emb_norm)
-            self.embedding.collect_params().setattr('grad_req', 'null') ## XXX - make this an option
+            if fixed_embedding:
+                self.embedding.collect_params().setattr('grad_req', 'null')
         ## Initialize decoder bias terms to corpus frequencies
         if wd_freqs:
             freq_nd = mx.nd.array(wd_freqs, ctx=ctx) + 1
@@ -70,7 +74,9 @@ class BowNTM(HybridBlock):
         """
         Encode data to the mean of the latent distribution defined by the input `data`
         """
-        return self.latent_dist.mu_encoder(self.encoder(self.embedding(data)))
+        mu = self.latent_dist.mu_encoder(self.encoder(self.embedding(data)))
+        norm = mx.nd.norm(mu, axis=1, keepdims=True)
+        return mu / norm
     
     def get_l1_penalty_term(self, F, l1_pen_const, batch_size):
         if F is mx.ndarray:

tmnt/distributions/logistic_gaussian.py
@@ -15,8 +15,8 @@ class LogisticGaussianLatentDistribution(LatentDistribution):
         self.alpha = 1.0
 
         prior_var = 1 / self.alpha - (2.0 / n_latent) + 1 / (self.n_latent * self.n_latent)
-        self.prior_var = prior_var
-        self.prior_logvar = math.log(prior_var)
+        self.prior_var = mx.nd.array([prior_var])
+        self.prior_logvar = mx.nd.array([math.log(prior_var)])
 
         with self.name_scope():
             self.mu_encoder = gluon.nn.Dense(units = n_latent, activation=None)
@@ -25,8 +25,8 @@ class LogisticGaussianLatentDistribution(LatentDistribution):
     def _get_kl_term(self, F, mu, lv):
         posterior_var = F.exp(lv)
         delta = mu
-        dt = delta * delta / self.prior_var
-        v_div = posterior_var / self.prior_var
+        dt = F.broadcast_div(delta * delta, self.prior_var)
+        v_div = F.broadcast_div(posterior_var, self.prior_var)
         lv_div = self.prior_logvar - lv
         return F.sum(0.5 * (F.sum((v_div + dt + lv_div), axis=1) - self.n_latent))
 