07d611332a031c469931284274098497d898d070
Wellner <wellner@.mitre.org>
2019-08-28 18:05:31 -0400

    Few changes to seq_vae

--- tmnt/seq_vae/trans_seq_models.py
+++ tmnt/seq_vae/trans_seq_models.py
@@ -126,12 +126,12 @@ class TransformerDecoder(HybridBlock):
             self.projection = nn.Dense(in_units = n_latent, units = wd_embed_dim)
             self.trans_block = TransformerBlock(
                 attention_cell = 'multi_head',
-                num_layers = 6,
+                num_layers = 8,
                 units = wd_embed_dim,  
-                hidden_size = 1024,
+                hidden_size = 512,
                 max_length = sent_size,
-                num_heads = 4,
-                scaled = True,
+                num_heads = 1,
+                scaled = False,
                 dropout = 0.0,
                 use_residual=True, output_attention=False,
                 ctx = ctx)
