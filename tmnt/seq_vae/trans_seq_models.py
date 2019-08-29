aabfa520d8f5d4e2da66ea101beeba2161f78f2e
Wellner <wellner@.mitre.org>
2019-08-29 15:41:43 -0400

    Match to bert word embedding

--- tmnt/seq_vae/trans_seq_models.py
+++ tmnt/seq_vae/trans_seq_models.py
@@ -71,8 +71,7 @@ class BertTransVAE(Block):
         ## BERT word embedding reconstruction experiment ...
         ## Let's move to cosine embedding loss over last dimension
         #prob_logits = self.inv_bert_embed(rec_y)
-        #emb_w = self.bert.word_embed.collect_params().get('weight').data()
-        emb_w = self.inv_bert_embed.get_weights()
+        emb_w = self.bert.word_embed[0].collect_params().get('weight').data()
         x = F.transpose(emb_w)
         y = rec_y
         x_norm = F.norm(w, axis=-1)
