1c48bd6bf57945ffbd27344f27f4f99f11f31801
Wellner <wellner@.mitre.org>
2019-09-03 18:10:57 -0400

    Create vocab dynamically from data

--- tmnt/seq_vae/trans_seq_models.py
+++ tmnt/seq_vae/trans_seq_models.py
@@ -51,10 +51,11 @@ class PureTransformerVAE(Block):
                                               batch_size = batch_size, ctx = ctx)
             self.decoder = TransformerDecoder(wd_embed_dim=self.wd_embed_dim, n_layers=dec_layers, n_latent=n_latent, sent_size = max_sent_len,
                                               batch_size = batch_size, ctx = ctx)
-            self.out_embedding = gluon.nn.Embedding(input_dim=self.vocab_size, output_dim=self.wd_embed_dim, weight_initializer=mx.init.Uniform(0.1))
+            self.out_embedding = gluon.nn.Embedding(input_dim=self.vocab_size, output_dim=self.wd_embed_dim)
             self.inv_embed = InverseEmbed(batch_size, max_sent_len, self.wd_embed_dim, temp=wd_temp, ctx=self.model_ctx, params = self.out_embedding.params)
             self.ce_loss_fn = mx.gluon.loss.SoftmaxCrossEntropyLoss(axis=-1, from_logits=True)
         self.embedding.initialize(mx.init.Xavier(magnitude=2.34), ctx=ctx)
+        self.out_embedding.initialize(mx.init.Xavier(magnitude=2.34), ctx=ctx)        
         self.inv_embed.initialize(mx.init.Xavier(magnitude=2.34), ctx=ctx)
         self.out_embedding.weight.set_data(self.vocabulary.embedding.idx_to_vec)
         self.embedding.weight.set_data(self.vocabulary.embedding.idx_to_vec)
