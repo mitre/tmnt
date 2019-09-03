fd5560c6fd62687e2ae68dd300cf1d2387d9ba9b
Wellner <wellner@.mitre.org>
2019-09-03 13:26:50 -0400

    Added non-bert seqvae encoder

--- tmnt/seq_vae/trans_seq_models.py
+++ tmnt/seq_vae/trans_seq_models.py
@@ -292,6 +292,7 @@ class TransformerEncoder(HybridBlock):
 
     def hybrid_forward(self, F, x):
         ## x is shape (N, sent_size, wd_embed_dim)
+        print("Shape of encoder input ... {}".format(x.shape))
         y, _ = self.trans_block(x)
         first = y[:,0,:]
         encoding = self.projection(first)
