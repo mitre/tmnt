d445f5020145f1c33ed3219e21641ca49f4c30e7
Wellner <wellner@.mitre.org>
2020-09-16 16:12:13 -0400

    Post init for BERT encoder

--- tmnt/models/seq_bow/models.py
+++ tmnt/models/seq_bow/models.py
@@ -155,8 +155,9 @@ class BertBowVED(Block):
             else:
                 raise Exception("Invalid distribution ==> {}".format(latent_distrib))
             self.decoder = gluon.nn.Dense(in_units=n_latent, units=self.bow_vocab_size, activation=None)
-        self.latent_dist.initialize(mx.init.Xavier(), ctx=self.model_ctx)
         self.decoder.initialize(mx.init.Xavier(), ctx=self.model_ctx)
+        self.latent_dist.initialize(mx.init.Xavier(), ctx=self.model_ctx)
+        self.latent_dist.post_init(self.model_ctx)
         if wd_freqs is not None:
             freq_nd = wd_freqs + 1
             total = freq_nd.sum()
