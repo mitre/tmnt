c24b9b95efc59c88a537fb6563c8eeda19fd9758
Wellner <wellner@.mitre.org>
2020-10-22 17:45:17 -0400

    Update import structure

--- tmnt/__init__.py
+++ tmnt/__init__.py
@@ -1,3 +1,11 @@
 # coding: utf-8
 
+from .classifier import *
+from .coherence import *
+from .distributions import *
+from .preprocess import *
+from .embeddings import *
+from .models import *
+from .utils import *
+
 __all__ = ['classifier', 'coherence', 'distributions', 'preprocess', 'embeddings', 'models', 'utils']
