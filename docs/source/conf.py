5448634d53b240d52dc44424a77ea2e897ae6a83
Wellner <wellner@.mitre.org>
2020-10-22 13:18:37 -0400

    Sphinx update to system path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./tmnt'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
