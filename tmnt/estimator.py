784c68a00181293cd9cfb480ba886cf9f5fbb0e4
Wellner <wellner@.mitre.org>
2021-03-16 09:07:59 -0400

    Added label mask option for seqbow

--- tmnt/estimator.py
+++ tmnt/estimator.py
@@ -1146,6 +1146,11 @@ class SeqBowEstimator(BaseEstimator):
                             valid_length.astype('float32').as_in_context(self.ctx), bow.as_in_context(self.ctx))
                         if self.has_classifier:
                             label_ls = self.loss_function(out, label.as_in_context(self.ctx)).mean()
+                            if len(label.shape) > 1:
+                                mask = labels.sum(axis=1) >= 0.0
+                            else:
+                                mask = labels >= 0.0
+                            label_ls = label_ls * mask
                             total_ls = (self.gamma * label_ls) + elbo_ls.mean()
                         else:
                             total_ls = elbo_ls.mean()
@@ -1240,8 +1245,8 @@ class SeqBowEstimator(BaseEstimator):
                 input_ids.as_in_context(self.ctx), type_ids.as_in_context(self.ctx),
                 valid_len.astype('float32').as_in_context(self.ctx), bow.as_in_context(self.ctx))
             if self.has_classifier:
-                ls = self.loss_function(out, label.as_in_context(self.ctx)).mean()
-                total_ls = (self.gamma * ls) + elbo_ls.mean()
+                label_ls = self.loss_function(out, label.as_in_context(self.ctx)).mean()
+                total_ls = (self.gamma * label_ls) + elbo_ls.mean()
             else:
                 total_ls = elbo_ls.mean()
             total_rec_loss += rec_ls.sum().asscalar()
