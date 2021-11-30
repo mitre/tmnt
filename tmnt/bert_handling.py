215cc20c823f91a05faff1d8e0220d32d90256d5
Wellner <wellner@.mitre.org>
2021-11-29 20:16:12 -0500

    Update to shuffling for bert data handling

--- tmnt/bert_handling.py
+++ tmnt/bert_handling.py
@@ -527,7 +527,7 @@ def get_dual_bert_datasets(class_labels,
         trans2s = [ get_transform(class_labels[i], max_len2) for i in range(len(train_ds2)) ]        
         train_sets = [
             preprocess_data_metriclearn_separate(trans1s[i], trans2s[i], class_labels[i], train_ds1[i], train_ds2[i],
-                                                 len(train_ds2[i]), shuffle_a_only=shuffle_a_only, shuffle_both=shuffle_both or shuffle)
+                                                 len(train_ds2[i]), shuffle_a_only=shuffle_a_only, shuffle_both=shuffle_both)
             for i in range(len(train_ds1)) ]
         num_train_examples = list(accumulate([ s for _,s in train_sets], lambda x,y: x + y)).pop()
         loaders = [l for l,_ in train_sets]
@@ -537,7 +537,7 @@ def get_dual_bert_datasets(class_labels,
         trans1 = get_transform(class_labels, max_len1)
         trans2 = get_transform(class_labels, max_len2)
         train_data, num_train_examples = preprocess_data_metriclearn_separate(
-            trans1, trans2, class_labels, train_ds1, train_ds2, batch_size, shuffle_a_only=shuffle)
+            trans1, trans2, class_labels, train_ds1, train_ds2, batch_size, shuffle_a_only=shuffle_a_only, shuffle_both=shuffle_both)
     if aux_dataset is not None:
         aux_trans = get_transform([], max_len1)
         aux_dataloader = get_aux_dataloader(aux_trans, aux_batch_size, aux_dataset)
