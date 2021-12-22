dd907bfa28a2295d1bb705f7f4fd073f14e5af1d
Wellner <wellner@.mitre.org>
2021-12-22 10:37:44 -0500

    No longer do full eval on each epoch by default

--- tmnt/estimator.py
+++ tmnt/estimator.py
@@ -1604,7 +1604,7 @@ class SeqBowEstimator(BaseEstimator):
             mx.nd.waitall()
 
             # inference on dev data
-            if dev_data is not None:
+            if dev_data is not None and (self.validate_each_epoch or epoch_id == self.epochs):
                 sc_obj, v_res = self._perform_validation(model, dev_data, epoch_id)
             else:
                 sc_obj, v_res = None, None
