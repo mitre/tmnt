7e920d3d019d0dfb206ee003e3064763c803da40
wellner <wellner@mitre.org>
2023-12-27 10:44:56 -0500

    Update to 0.7.42 with fix to readthedocs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from pathlib import Path
 
-version = '0.7.41'
+version = '0.7.42'
 
 try:
     if not os.getenv('RELEASE'):
