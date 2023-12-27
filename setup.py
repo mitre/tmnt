b689c1b9bc466640d17509047919d679c1ecfa5e
wellner <wellner@mitre.org>
2023-12-27 10:12:03 -0500

    New stable release at 0.7.41

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from pathlib import Path
 
-version = '0.7.04'
+version = '0.7.41'
 
 try:
     if not os.getenv('RELEASE'):
