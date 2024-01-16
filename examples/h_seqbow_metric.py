Update to fix gradient clipping after gradients removed

examples/h_seqbow_metric.py
@@ -16,7 +16,7 @@ data, y = fetch_20newsgroups(shuffle=True, random_state=1,
                               remove=('headers', 'footers', 'quotes'),
                               return_X_y=True)
 
-tr_size = 1000
+tr_size = 200
 train_data_a = data[:tr_size]
 dev_data_a   = data[-tr_size:]
 train_y_a    = y[:tr_size]
@@ -62,9 +62,9 @@ device = torch.device('cpu')
 
 #train_loader = get_llm_paired_dataloader(train_ds_a[:100], train_ds_a[:100], vectorizer, tf_llm_name, label_map, 20, 256, 256 , shuffle_both=True, device=device)
 #train_loader = StratifiedPairedLLMLoader(train_ds_a, train_ds_b, vectorizer, tf_llm_name, label_map, 20, 512, 512, device=device)
-train_loader = StratifiedPairedLLMLoader(train_ds_a, train_ds_b, vectorizer, tf_llm_name, label_map, 20, 128, 128, device=device)
+train_loader = StratifiedPairedLLMLoader(train_ds_a, train_ds_b, vectorizer, tf_llm_name, label_map, 20, 64, 64, device=device)
 #dev_loader = get_llm_paired_dataloader(dev_ds_a, dev_ds_b, vectorizer, tf_llm_name, label_map, 50, 256, 256, device=device)
-dev_loader = StratifiedPairedLLMLoader(dev_ds_a, dev_ds_b, vectorizer, tf_llm_name, label_map, 20, 512, 512, device=device)
+dev_loader = StratifiedPairedLLMLoader(dev_ds_a, dev_ds_b, vectorizer, tf_llm_name, label_map, 20, 64, 64, device=device)
 #aux_loader = get_llm_dataloader(aux_ds, vectorizer, tf_llm_name, label_map, 10, 128, shuffle=True, device=device) 
 
 #latent_distribution = LogisticGaussianDistribution(768,10,dr=0.05,alpha=1.0,device=device)

tmnt/distribution.py
@@ -45,20 +45,11 @@ class BaseDistribution(nn.Module):
     def _get_unit_var_gaussian_sample(self, mu, batch_size):
         eps = Normal(torch.zeros(batch_size, self.n_latent), torch.ones(batch_size, self.n_latent)).sample()
         return (mu + eps).to(self.device)
+    
+    def get_mu_encoding(self, data, include_bn):
+        raise NotImplemented 
 
-    def get_mu_encoding(self, data, include_bn=False):
-        """Provide the distribution mean as the natural result of running the full encoder
-        
-        Parameters:
-            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
-        Returns:
-            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
-        """
-        enc = self.mu_encoder(data)
-        if include_bn:
-            enc = self.mu_bn(enc)
-        return self.softplus(enc)
-        
+
 
 
 class GaussianDistribution(BaseDistribution):
@@ -91,6 +82,20 @@ class GaussianDistribution(BaseDistribution):
         KL = self._get_kl_term(mu_bn, lv_bn)
         z = self.post_sample_dr_o(z)
         return z, KL
+    
+    def get_mu_encoding(self, data, include_bn=False):
+        """Provide the distribution mean as the natural result of running the full encoder
+        
+        Parameters:
+            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
+        Returns:
+            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
+        """
+        enc = self.mu_encoder(data)
+        if include_bn:
+            enc = self.mu_bn(enc)
+        return self.softplus(enc)
+        
 
 
 class GaussianUnitVarDistribution(BaseDistribution):
@@ -120,7 +125,20 @@ class GaussianUnitVarDistribution(BaseDistribution):
         z = self._get_gaussian_sample(mu_bn, self.log_variance, batch_size)
         KL = self._get_kl_term(mu_bn)
         return self.post_sample_dr_o(z), KL
-
+    
+    def get_mu_encoding(self, data, include_bn=False):
+        """Provide the distribution mean as the natural result of running the full encoder
+        
+        Parameters:
+            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
+        Returns:
+            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
+        """
+        enc = self.mu_encoder(data)
+        if include_bn:
+            enc = self.mu_bn(enc)
+        return self.softplus(enc)
+        
 
 class LogisticGaussianDistribution(BaseDistribution):
     """Logistic normal/Gaussian latent distribution with specified prior
@@ -164,6 +182,19 @@ class LogisticGaussianDistribution(BaseDistribution):
         z = self.post_sample_dr_o(z_p)
         return self.softmax(z), KL
 
+    def get_mu_encoding(self, data, include_bn=False):
+        """Provide the distribution mean as the natural result of running the full encoder
+        
+        Parameters:
+            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
+        Returns:
+            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
+        """
+        enc = self.mu_encoder(data)
+        if include_bn:
+            enc = self.mu_bn(enc)
+        return self.softmax(enc)
+        
     
 class VonMisesDistribution(BaseDistribution):
     
@@ -188,6 +219,20 @@ class VonMisesDistribution(BaseDistribution):
         z_p = VonMises(mu_bn, self.kappa).sample()
         kld = self.kld_v.expand(batch_size)
         return z_p, kld
+
+    def get_mu_encoding(self, data, include_bn=False):
+        """Provide the distribution mean as the natural result of running the full encoder
+        
+        Parameters:
+            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
+        Returns:
+            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
+        """
+        enc = self.mu_encoder(data)
+        if include_bn:
+            enc = self.mu_bn(enc)
+        return self.softplus(enc)
+        
     
 
 class Projection(BaseDistribution):
@@ -202,6 +247,19 @@ class Projection(BaseDistribution):
         kld = torch.zeros(batch_size).to(self.device)
         return mu_bn, kld
 
+    def get_mu_encoding(self, data, include_bn=False):
+        """Provide the distribution mean as the natural result of running the full encoder
+        
+        Parameters:
+            data (:class:`mxnet.ndarray.NDArray`): Output of pre-latent encoding layers
+        Returns:
+            encoding (:class:`mxnet.ndarray.NDArray`): Encoding vector representing unnormalized topic proportions
+        """
+        enc = self.mu_encoder(data)
+        if include_bn:
+            enc = self.mu_bn(enc)
+        return enc
+        
         
     
 

tmnt/estimator.py
@@ -1465,11 +1465,10 @@ class SeqBowEstimator(BaseEstimator):
                     update_loss_details(total_ls_2, elbo_ls_2, red_ls_2, None)
 
                 if not accumulate or (batch_id + 1) % accumulate == 0:
+                    torch.nn.utils.clip_grad.clip_grad_value_(model.llm.parameters(), 1.0)
                     lr_scheduler.step()
                     dec_optimizer.step()
                     model.zero_grad()
-                    # clip gradients for the underlying LLM Transformer-based encoder
-                    torch.nn.utils.clip_grad.clip_grad_value_(model.llm.parameters(), 1.0)
                     step_num += 1
                 if (batch_id + 1) % (self.log_interval) == 0:
                     lr = lr_scheduler.get_last_lr()[0] # get lr from first group