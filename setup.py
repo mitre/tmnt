22d5ca519f65d905883bbf66f7b90eb9f92e0ae9
wellner <wellner@mitre.org>
2025-03-13 12:22:48 -0400

    Bumped version to 0.7.57

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from pathlib import Path
 
-version = '0.7.56'
+version = '0.7.57'
 
 try:
     if not os.getenv('RELEASE'):
