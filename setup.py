cfc937216ae107d49f9406ec937f5f7ec77a7c8e
wellner <wellner@mitre.org>
2025-04-18 17:46:27 -0400

    Bump to 59

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from pathlib import Path
 
-version = '0.7.58'
+version = '0.7.59'
 
 try:
     if not os.getenv('RELEASE'):
